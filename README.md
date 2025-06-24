f8a99f4d1a222e9ab830f054388e0a2f6ac5ea6e
PaulFilms <pablopila.spain@gmail.com>
2025-06-24 15:58:50 +0200

    Refactored

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # PyReports
 Toolkit for Documentation and Reporting [ALPHA VERSION]
 
+![Last commit](https://img.shields.io/github/last-commit/PaulFilms/PyReports?label=Último%20commit)
+
+
 ## Tools included
 - Report Spreadsheets .xlsx Files
 - Report .PDF Files
 
 
-## Installation Method
+## 🔧 Installation
 
 - Last released version
 
@@ -20,13 +23,29 @@ Toolkit for Documentation and Reporting [ALPHA VERSION]
    pip install git+https://github.com/PaulFilms/PyReports.git@main
    ```
 
+## 📦 Dependencies
+
+This project relies on the following open-source libraries:
+
+| Package      | License      | Description                                                   |
+|--------------|---------------|---------------------------------------------------------------|
+| `pandas`     | BSD 3-Clause  | High-performance data manipulation and analysis.              |
+| `openpyxl`   | MIT           | Read and write Excel `.xlsx` files.                           |
+| `reportlab`  | BSD           | Dynamic PDF generation from Python.                           |
+---
+
+
+
+
 
 ## TASK 📒
 
-- ...
+- re-define PDFREPORT and XLSREPORT with standards formats (normal, header 1 ... 3, caption, etc)
+- PDF, re-define canvas size
+- PDF, text size greater than the width of the canvas
 
 
 
 ## WARNINGS ⛔
 
-- ...
+- PDFREPORT its under construction yet
--- pyreports/pdf.py
+++ pyreports/pdf.py
@@ -1,13 +1,15 @@
 '''
 Toolkit with simplified functions and methods for create .pdf Reports
 
-Comming Soon
+⚠️INCOMPLETE
 '''
-__update__ = '2024.09.27'
+__update__ = '2025.06.24'
 
 from dataclasses import dataclass
+from enum import Enum
 
 from reportlab.pdfgen import canvas
+from reportlab.lib.units import cm, inch
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from reportlab.lib import pagesizes
@@ -18,34 +20,80 @@ from reportlab.lib.utils import ImageReader
 ## TOOLS
 ## _________________________________________________________________________________________________________________
 
-@dataclass
-class fontTypes(str):
-	'''
-	DataClass to define Font Types in text format
-	'''
-	normal: str = "Normal"
-	bold: str = "Bold"
-	italic: str = "Italic"
-	bold_italic: str = "Bold_Italic"
+''' ❕PAGE SIZE:
+In ReportLab, the size of your Canvas (and all coordinates/dimensions you use) is expressed in points, 
+where 1 point = 1/72 of an inch
 
-@dataclass
-class richText():
+from reportlab.lib.units import cm, inch
+
+print(cm)    # ≈ 28.35 point
+print(inch)  # 72 point
+'''
+
+class orientations(Enum):
+	landscape = 'landscape'
+	portrait = 'portrait'
+
+class pase_sizes(Enum):
 	'''
-	DataClass for defining the content and formatting of a text string
+	Tuple[x, y]
 	'''
-	value: str = ""
-	font: fontTypes = fontTypes.normal
-	size: float = 8
-	color: colors = colors.black
+	A4_horizontal = (cm*21, cm*29.7) # 210 x 297 mm
+	A4_vertical = (cm*29.7, cm*21) # 297 x 210 mm
+
+# @dataclass
+# class fontTypes(str):
+# 	'''
+# 	DataClass to define Font Types in text format
+# 	'''
+# 	normal: str = "Normal"
+# 	bold: str = "Bold"
+# 	italic: str = "Italic"
+# 	bold_italic: str = "Bold_Italic"
+
+class fontTypes(Enum):
+	'''
+	Font Types in text format
+	'''
+	normal = TTFont('normal', 'arial.ttf')
+	bold = TTFont('bold', 'arialbd.ttf')
+	italic = TTFont('italic', 'ariali.ttf')
+	italic_bold = TTFont('italic_bold', 'arialbi.ttf')
+
+# @dataclass
+# class richText():
+# 	'''
+# 	DataClass for defining the content and formatting of a text string
+# 	'''
+# 	value: str = ""
+# 	font: fontTypes = fontTypes.normal
+# 	size: float = 8
+# 	color: colors = colors.black
 		
 class PDFREPORT:
 	'''
 	Un A-4 a 72 ppp    595 x 842
 	Default Font: Arial
 	'''
-	def __init__(self, filePath, docTitle: str, defaultFont=fontTypes.normal, defaultSize=8, defaulColor=colors.black):
-		self.filePath = filePath
-		self.PDF = canvas.Canvas(self.filePath + ".pdf")
+	def __init__(self, 
+			filePath, 
+			docTitle: str, 
+			# defaultFont=fontTypes.normal, 
+			# defaultSize=8, 
+			defaulColor=colors.black,
+			# orientation: orientations = orientations.portrait.value,
+			pagesize: tuple[float, float] = pagesizes.portrait(pagesizes.A4), ## Tuple[x: float, y: float]
+			marginTop: float = cm*1, ## 1 cm
+			marginBottom: float = cm*2, ## 1 cm
+			marginLeft: float = cm*1.5, ## 1.5 cm
+			marginRight: float = cm*1, ## 1 cm
+		):
+		
+		# self.filePath = filePath
+		self.PDF = canvas.Canvas(
+			filePath,
+			pagesize=pagesize
+		)
 
 		## DOCUMENT NAME
 		if docTitle:
@@ -54,28 +102,17 @@ class PDFREPORT:
 			self.PDF.setTitle("REPORT")
 		
 		## FONTS
-		# pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
-		# pdfmetrics.registerFont(TTFont('Times_new-BoldItalic', 'timesbi.ttf', subfontIndex=3))
-		pdfmetrics.registerFont(TTFont('Normal', 'arial.ttf'))
-		pdfmetrics.registerFont(TTFont('Bold', 'arialbd.ttf'))
-		pdfmetrics.registerFont(TTFont('Italic', 'ariali.ttf'))
-		pdfmetrics.registerFont(TTFont('Bold_Italic', 'arialbi.ttf'))
-		self.defaultFont = defaultFont
-		self.defaultSize = defaultSize
-		self.defaulColor = defaulColor
-		# self.SET_FONT(fontTypes.normal, 8)
-		self.SET_DEFAULT()
+		for f in fontTypes:
+			pdfmetrics.registerFont(f.value)
 		
 		## PAGE SIZE / MARGINS
-		pageSize = pagesizes.landscape(pagesizes.A4)
-		self.pageHeight = pageSize[0] # 841.88
-		self.pageWidth = pageSize[1] # 595.27
-		self.marginLeft = 45
-		self.marginRight = self.pageWidth - self.marginLeft
-		self.marginTop = 10
-		self.marginBottom = self.pageHeight - 10
-		self.row = 820 - self.marginTop
 		self.spacing = 5
+		# self.pageSize = pagesize
+		self.marginTop = marginTop
+		self.marginBottom = marginBottom
+		self.marginLeft = marginLeft
+		self.marginRight = marginRight
+
 
 		## Get objects
 		# lineWidth = self.PDF._lineWidth
@@ -85,140 +122,218 @@ class PDFREPORT:
 
 		## SAVING THE .PDF FILE
 		# self.PDF.save()
+
+		## Cursor Position
+		self.X = 0
+		self.Y = 0
 		pass
 
-	def WR_LINE(self, x=int, y=int, TXT=str):
-		'''
-		Writte a line text with the default font config
-		'''
-		xpos = x
-		ypos = y
-		words = TXT.split(chr(32))
-		for word in words:
-			wordWidth = pdfmetrics.stringWidth(word + " ", self.PDF._fontname, self.PDF._fontsize)
-			if xpos + wordWidth > self.marginRight:
-				xpos = self.marginLeft
-				ypos -= (self.defaultSize + self.spacing)
-			self.PDF.drawString(xpos,ypos,word)
-			xpos += wordWidth + 1
-		self.row = ypos - (self.defaultSize + self.spacing)
+	def showPage(self):
+		self.PDF.showPage()
+
+	def save(self):
+		self.PDF.save()
 	
-	def WR_LINE_CENTERED(self, x=int, y=int, TXT=str):
-		self.PDF.drawCentredString(x,y,TXT)
-		self.row -= (self.PDF._fontsize + self.spacing)
+	def get_x(self, centered: bool = False) -> float:
+		if centered:
+			return (( self.PDF._pagesize[0] - self.marginLeft - self.marginRight ) / 2 ) + self.marginLeft
+		else:
+			return self.marginLeft
 	
-	def WR_MULTILINE(self):
-		'''
-		INCOMPLETE
-		'''
-		# text = self.PDF.beginText(40, 680)
-		# text.setFont("Courier", 18)
-		# text.setFillColor(colors.red)
-		# for line in textLines:
-		# 	text.textLine(line)
-		# self.PDF.drawText(text)
-		pass
+	def get_y(self) -> float:
+		return self.PDF._pagesize[1] - self.marginTop
 
-	def WR_HEADER(self, x=int, y=int, TXT=str, filling=None, fontType=fontTypes.bold, size=15):
-		'''
-		hay que controlar el ancho del HEADER
-		'''
-		ypos = y - size
-		## FILLING
-		self.PDF.setFillColorRGB(100, 100, 100)
-		# textWidth = pdfmetrics.stringWidth(TXT + "  ", fontType, size)
-		self.PDF.setLineWidth(0)
-		self.PDF.setLineCap(0)
-		# self.PDF.rect(report.marginLeft,y,textWidth,size, fill=1)
-		# self.PDF.rect(self.marginLeft,ypos,self.marginRight-30,size, fill=1)
-		## TEXT
-		self.PDF.setFillColor(colors.black)
-		self.PDF.setFont(fontType, size)
-		self.PDF.drawString(x,ypos,TXT)
-		self.row = ypos
-		self.WR_SPACING(2)
-		## SET DEFAULT
-		self.SET_DEFAULT()
+	def wr_divider(self, y: float, line_width: float = 1.5) -> tuple[float, float]:
+		self.PDF.setLineWidth(line_width)
+		self.PDF.line(
+			x1=self.marginLeft, 
+			y1=y, 
+			x2=self.PDF._pagesize[0] - self.marginRight, 
+			y2=y
+		)
+		return self.get_x(), y - 20
 
-	def WR_RICHTEXT(self, x=int, y=int, TXT_LIST=list):
-		'''
-		Escribe una linea tanto en formato str como con richText respetando el ancho de la página
-		'''
-		xpos = x
-		ypos = y
-		for item in TXT_LIST:
-			# Set Text
-			if type(item) == str:
-				txt = item
-			if type(item) == int or type(item) == float:
-				txt = str(item)
-			if type(item) == richText:
-				self.SET_FONT(item.font, item.size)
-				self.SET_COLOR(item.color)
-				txt = str(item.value)
-			# 
-			words = txt.split(chr(32))
-			for word in words:
-				wordWidth = pdfmetrics.stringWidth(word + " ", self.PDF._fontname, self.PDF._fontsize)
-				if xpos + wordWidth > self.marginRight:
-					xpos = self.marginLeft
-					ypos -= (self.defaultSize + self.spacing)
-				self.PDF.drawString(xpos,ypos,word)
-				xpos += wordWidth + 1
-			# Return to default font config
-			self.SET_DEFAULT()
-		# 
-		self.row = ypos - (self.defaultSize + self.spacing)
+	def wr_normal(self, y: float, text: str, centered: bool = False) -> tuple[float, float]:
+		font_name = fontTypes.normal.name
+		font_size = 12
+		self.PDF.setFont(font_name, font_size, leading=font_size + self.spacing)
+		
+		x=self.get_x()
+		if centered:
+			text_width = self.PDF.stringWidth(text, self.PDF._fontname, self.PDF._fontsize)
+			x = self.get_x(centered=centered) - (text_width / 2)	
 
-	def WR_HLINE(self, y=int, lineWidth: int=1.5):
-		'''
-		Draw a horizontal line
-		'''
-		self.PDF.setLineWidth(lineWidth) # Line Width
-		self.PDF.line(self.marginLeft, y, self.marginRight, y)
-		self.WR_SPACING(2)
+		# Construye el TextObject
+		txt = self.PDF.beginText()
+		txt.setTextOrigin(x, y)
+		txt.setFont(font_name, font_size)
+		txt.setLeading(font_size + self.spacing)
+		txt.textLine(text)
+		self.PDF.drawText(txt)
 
-	def WR_SPACING(self, lines=1):
-		self.row -= (self.spacing * 2 * lines)
+		# total_spacing = self.spacing + self.PDF._fontsize
+		return 0.0, txt.getY() # y - total_spacing
 
-	def IMAGE_INSERT(self, imageFile, x=int, y=int, ):
-		'''
-		INCOMPLETE
-		Hay que añadir la relacion de aspecto
-		'''
-		## drawing a image at the specified (x.y) position
-		image = ImageReader(imageFile)
-		image = imageFile
-		self.PDF.drawInlineImage(image, x, y)
-		pass
+	def wr_header1(self, y: float, text: str, centered: bool = False) -> tuple[float, float]:
+		font_name = fontTypes.bold.name
+		font_size = 14
+		self.PDF.setFont(font_name, font_size, leading=font_size + self.spacing)
+		
+		x=self.get_x()
+		if centered:
+			text_width = self.PDF.stringWidth(text, self.PDF._fontname, self.PDF._fontsize)
+			x = self.get_x(centered=centered) - (text_width / 2)	
 
-	def FONT_REGISTRER(self, fontName=str, fontTTF="Arial.ttf"):
-		pdfmetrics.registerFont(TTFont(fontName, fontTTF))
+		# Construye el TextObject
+		txt = self.PDF.beginText()
+		txt.setTextOrigin(x, y - self.spacing)
+		txt.setFont(font_name, font_size)
+		txt.setLeading(font_size + self.spacing)
+		txt.textLine(text)
+		self.PDF.drawText(txt)
+
+		# total_spacing = self.spacing + self.PDF._fontsize
+		return 0.0, txt.getY() # - self.spacing # y - total_spacing
+
+
+	# def WR_LINE(self, x=int, y=int, TXT=str):
+	# 	'''
+	# 	Writte a line text with the default font config
+	# 	'''
+	# 	xpos = x
+	# 	ypos = y
+	# 	words = TXT.split(chr(32))
+	# 	for word in words:
+	# 		wordWidth = pdfmetrics.stringWidth(word + " ", self.PDF._fontname, self.PDF._fontsize)
+	# 		if xpos + wordWidth > self.marginRight:
+	# 			xpos = self.marginLeft
+	# 			ypos -= (self.defaultSize + self.spacing)
+	# 		self.PDF.drawString(xpos,ypos,word)
+	# 		xpos += wordWidth + 1
+	# 	self.row = ypos - (self.defaultSize + self.spacing)
 	
-	def SET_DEFAULT(self):
-		'''
-		Return to default font config
-		'''
-		self.SET_FONT(self.defaultFont, self.defaultSize)
-		self.PDF.setFillColor(self.defaulColor)
+	# def WR_LINE_CENTERED(self, x=int, y=int, TXT=str):
+	# 	self.PDF.drawCentredString(x,y,TXT)
+	# 	self.row -= (self.PDF._fontsize + self.spacing)
+	
+	# def WR_MULTILINE(self):
+	# 	'''
+	# 	INCOMPLETE
+	# 	'''
+	# 	# text = self.PDF.beginText(40, 680)
+	# 	# text.setFont("Courier", 18)
+	# 	# text.setFillColor(colors.red)
+	# 	# for line in textLines:
+	# 	# 	text.textLine(line)
+	# 	# self.PDF.drawText(text)
+	# 	pass
 
-	def SET_FONT(self, fontType: fontTypes, fontSize: int):
-		'''
-		Set the font Type and font Size
-		'''
-		self.PDF.setFont(fontType, fontSize)
+	# def WR_HEADER(self, x=int, y=int, TXT=str, filling=None, fontType=fontTypes.bold, size=15):
+	# 	'''
+	# 	hay que controlar el ancho del HEADER
+	# 	'''
+	# 	ypos = y - size
+	# 	## FILLING
+	# 	self.PDF.setFillColorRGB(100, 100, 100)
+	# 	# textWidth = pdfmetrics.stringWidth(TXT + "  ", fontType, size)
+	# 	self.PDF.setLineWidth(0)
+	# 	self.PDF.setLineCap(0)
+	# 	# self.PDF.rect(report.marginLeft,y,textWidth,size, fill=1)
+	# 	# self.PDF.rect(self.marginLeft,ypos,self.marginRight-30,size, fill=1)
+	# 	## TEXT
+	# 	self.PDF.setFillColor(colors.black)
+	# 	self.PDF.setFont(fontType, size)
+	# 	self.PDF.drawString(x,ypos,TXT)
+	# 	self.row = ypos
+	# 	self.WR_SPACING(2)
+	# 	## SET DEFAULT
+	# 	self.SET_DEFAULT()
 
-	def SET_COLOR(self, color=colors, rgb=tuple):
-		'''
-		INCOMPLETE:
-			- Hay que crear un dataclass con el color
-		'''
-		# if color == "black": self.PDF.setFillColorRGB(0, 0, 0)
-		# if color == "blue": self.PDF.setFillColorRGB(0, 0, 255)
-		# if color == "red": self.PDF.setFillColorRGB(255, 0, 0)
-		# if color == "green": self.PDF.setFillColorRGB(0, 255, 0)
-		# if color == "grey": self.PDF.setFillColorRGB(160, 160, 160)
-		self.PDF.setFillColor(color)
-		color_rgb = self.PDF._fillColorObj
-		# print("SET_COLOR:", color_rgb)
-		pass
+	# def WR_RICHTEXT(self, x=int, y=int, TXT_LIST=list):
+	# 	'''
+	# 	Escribe una linea tanto en formato str como con richText respetando el ancho de la página
+	# 	'''
+	# 	xpos = x
+	# 	ypos = y
+	# 	for item in TXT_LIST:
+	# 		# Set Text
+	# 		if type(item) == str:
+	# 			txt = item
+	# 		if type(item) == int or type(item) == float:
+	# 			txt = str(item)
+	# 		if type(item) == richText:
+	# 			self.SET_FONT(item.font, item.size)
+	# 			self.SET_COLOR(item.color)
+	# 			txt = str(item.value)
+	# 		# 
+	# 		words = txt.split(chr(32))
+	# 		for word in words:
+	# 			wordWidth = pdfmetrics.stringWidth(word + " ", self.PDF._fontname, self.PDF._fontsize)
+	# 			if xpos + wordWidth > self.marginRight:
+	# 				xpos = self.marginLeft
+	# 				ypos -= (self.defaultSize + self.spacing)
+	# 			self.PDF.drawString(xpos,ypos,word)
+	# 			xpos += wordWidth + 1
+	# 		# Return to default font config
+	# 		self.SET_DEFAULT()
+	# 	# 
+	# 	self.row = ypos - (self.defaultSize + self.spacing)
+
+	# def WR_HLINE(self, y=int, lineWidth: int=1.5):
+	# 	'''
+	# 	Draw a horizontal line
+	# 	'''
+	# 	self.PDF.setLineWidth(lineWidth) # Line Width
+	# 	self.PDF.line(self.marginLeft, y, self.marginRight, y)
+	# 	self.WR_SPACING(2)
+
+	# def WR_SPACING(self, lines=1):
+	# 	self.row -= (self.spacing * 2 * lines)
+
+	# def IMAGE_INSERT(self, imageFile, x=int, y=int, ):
+	# 	'''
+	# 	INCOMPLETE
+	# 	Hay que añadir la relacion de aspecto
+	# 	'''
+	# 	## drawing a image at the specified (x.y) position
+	# 	image = ImageReader(imageFile)
+	# 	image = imageFile
+	# 	self.PDF.drawInlineImage(image, x, y)
+	# 	pass
+
+	# def FONT_REGISTRER(self, fontName=str, fontTTF="Arial.ttf"):
+	# 	pdfmetrics.registerFont(TTFont(fontName, fontTTF))
+	
+	# def SET_DEFAULT(self):
+	# 	'''
+	# 	Return to default font config
+	# 	'''
+	# 	self.SET_FONT(self.defaultFont, self.defaultSize)
+	# 	self.PDF.setFillColor(self.defaulColor)
+
+	# def SET_FONT(self, fontType: fontTypes, fontSize: int):
+	# 	'''
+	# 	Set the font Type and font Size
+	# 	'''
+	# 	self.PDF.setFont(fontType, fontSize)
+
+	# def SET_COLOR(self, color=colors, rgb=tuple):
+	# 	'''
+	# 	INCOMPLETE:
+	# 		- Hay que crear un dataclass con el color
+	# 	'''
+	# 	# if color == "black": self.PDF.setFillColorRGB(0, 0, 0)
+	# 	# if color == "blue": self.PDF.setFillColorRGB(0, 0, 255)
+	# 	# if color == "red": self.PDF.setFillColorRGB(255, 0, 0)
+	# 	# if color == "green": self.PDF.setFillColorRGB(0, 255, 0)
+	# 	# if color == "grey": self.PDF.setFillColorRGB(160, 160, 160)
+	# 	self.PDF.setFillColor(color)
+	# 	color_rgb = self.PDF._fillColorObj
+	# 	# print("SET_COLOR:", color_rgb)
+	# 	pass
+
+def from_markdown(md_path: str, pdf_path: str):
+	''' ⚠️INCOMPLETE
+	Create an .pdf document from MarkDown File
+	'''
+	pass
